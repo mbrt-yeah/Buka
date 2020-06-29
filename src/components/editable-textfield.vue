ab986aa9ee556fa852e78f4a92e85aee237fd13d
matthias.einbrodt@unibz.it <matthias.einbrodt@unibz.it>
2020-06-29 09:37:27 +0200

    Adds standalone mode

--- src/components/editable-textfield.vue
+++ src/components/editable-textfield.vue
@@ -2,13 +2,42 @@
     <div class="editable-textfield-component" v-bind:class="customClasses">
         <label v-if="!hideLabel" :for="id" class="label">{{ label }}:</label>
         <div class="textfield">
-            <div class="edit-mode" v-if="isEditMode">
-                <input type="text" :id="id" v-model="value" :name="name" :placeholder="label" @keyup="onValueChange" />
-            </div>
-            <div class="display-mode" v-else>
-                <p class="displayed-value" v-if="value">{{value}}</p>
-                <p class="displayed-value" v-else>{{ placeholder }}</p>
-            </div>
+            <template v-if="isStandalone">
+                <div v-if="isEditMode" class="edit-mode">
+                    <input type="text" :id="id" v-model="value" :name="name" :placeholder="label" />
+                    <button type="button" class="button button-small button-text button-icon-only button-positive" @click="onSaveClick">
+                        <span class="iconmonstr iconmonstr-buka-save"></span>
+                        <span class="text">{{ $t('Save') }}</span>
+                    </button>
+                    <button type="button" class="button button-small button-icon-only button-text" @click="onCancelClick">
+                        <span class="iconmonstr iconmonstr-buka-x-mark"></span>
+                        <span class="text">{{ $t('Cancel') }}</span>
+                    </button>
+                </div>
+                <div v-else class="display-mode">
+                    <p class="displayed-value" v-if="value">{{value}}</p>
+                    <p class="displayed-value" v-else>{{ placeholder }}</p>
+                    <button type="button" class="button button-small button-text button-icon-only document-list-edit-button" @click="onEditClick">
+                        <span class="iconmonstr iconmonstr-buka-pencil"></span> 
+                        <span class="text">{{ $t('Edit') }}</span>
+                    </button>
+                    <button type="button" class="button button-small button-icon-only button-text button-negative document-list-delete-button" @click="onDeleteClick">
+                        <span class="iconmonstr iconmonstr-buka-trash-can"></span>
+                        <span class="text">{{ $t('Delete') }}</span>
+                    </button>
+                </div>
+            </template>
+            <template v-else>
+                <div class="textfield">
+                    <div v-if="isEditMode" class="edit-mode">
+                        <input type="text" :id="id" v-model="value" :name="name" :placeholder="label" @keyup="onValueChange" />
+                    </div>
+                    <div v-else class="display-mode">
+                        <p class="displayed-value" v-if="value">{{value}}</p>
+                        <p class="displayed-value" v-else>{{ placeholder }}</p>
+                    </div>
+                </div>
+            </template>
         </div>
     </div>
 </template>
@@ -51,11 +80,20 @@
             this.id = uuid.generate();
         }
 
+        public onCancelClick() {
+            this.isEditMode = false;
+        }
+
+        public onDeleteClick() {
+            this.$emit('delete');
+        }
+
         public onEditClick() {
             this.isEditMode = true;
         }
 
         public onSaveClick() {
+            this.$emit('change', new EditableTextfieldChangeEvent(this.name, this.value));
             this.isEditMode = false;
         }
 
